a269fb96b65ed6a6dc615d6ce6e9520b0eee5478
githjohn <1693886289@qq.com>
2017-11-28 12:20:09 +0800

    python课件
    python教学课件

---
+++ fullstack/week1/day3/用户交互.py
@@ -0,0 +1,21 @@
+# #coding:utf-8 python3默认是utf-8  默认是3个字节
+# #要是python2  就要設置 #! -*- coding:gbk -*-
+# age_of_princal = 56
+# guess_age = int(   input(">>:") )
+# if guess_age == age_of_princal:
+#     print("Yes,you got it..")
+# elif guess_age > age_of_princal:
+#     print("shoud try samller..")
+# else:
+#     print("try bigger ...")
+# #------------------------------
+# death_age = 80
+# name = input("your name:")
+# age = input("your age:")  #input 接受的所有数据都是字符串，即便你输入的是数字，但依然会被当成字符串来处理
+# print( type(age) )  #查看类型
+# #int integer =整数  把字符串转成int,用int(被转的数据)
+# #str string =字符串 把数据转成字h符串用str(被转的数据)
+# print("Your name:",name)
+# #拼接用+必須是字符串才可以
+# print("You can still live for " +  str(death_age - int(age)) +" years ....")
+
---
+++ fullstack/week2/day1/login.py
@@ -0,0 +1,14 @@
+for i in range(1,101,2): #2 步长
+    print("loop:",i)
+# passed_authentication = False  #假，不成立
+# for i in range(3):
+#     username = input("Username:")
+#     password = input("Password:")
+#     if username == _user and password == _passwd :
+#         print("Welcome %s login...." % _user)
+#         passed_authentication = True #，真，成立
+#         break #跳出，中断
+#     else:
+#         print("Invalid username or password !")
+# if not passed_authentication:#只有在True的情况下，条件成立
+#     print("要不要脸，臭流氓啊，小虎。")
