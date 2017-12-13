cb483a6ec9e039017610892332f28b99072866c5
githjohn <1693886289@qq.com>
2017-12-13 20:19:14 +0800

    Update list_lesson.py

--- fullstack/week2/day1/list_lesson.py
+++ fullstack/week2/day1/list_lesson.py
@@ -1,93 +1,44 @@
-# a=['wuchao','jinxin','xiaohu','sanpang','ligang']
-# #,['wuchao','jinxin']
+#a=['wuchao','jinxin','xiaohu','sanpang','ligang']
 # #-1是一直取到低
-# #增删改查
 # #查  切片 []
-# # print(a[1:])#取到最后
-# print(a[1:-1])#取到倒数第二值
-# print(a[1:-1:1])#从左到右一个一个去取
-# # print(a[1::2])#从左到右隔一个去取
-# #print(a[3::-1])
-# # b=a[3::-1]
-# # print(b)#['sanpang', 'xiaohu', 'jinxin', 'wuchao']
-# #print(a[-2::-1])
-# #添加 append insert
-# # a.append('xuepeng')  #默认插到最后一个位置
-# # print(a)
-# # a.insert(1,'xuepeng') #将数据插入到任意一个位置
-# # print(a)
-#
-# #修改
-# #
-# # a[1]='haidilao'
-# # print(a)
-# # a[1:3]=['a','b']
-# # print(a)
-#
-#
-# #删除 remove pop del
-# # c=["a","a","a","a"]
-# # c.remove("a") #只删除一个
-# # print(c)
-# # a.remove(a[0])
-# # print(a)
-# b=a.pop(1) #会返回删除的值
-# print(a)
-# print(b)
-# #
-# # del a[0]
-# # print(a)
-# # del a
-# # print(a)
-# # a.remove(['wuchao','jinxin'])
-# # print(a)
-#
-#
-# #count:计算某元素出现次数
-# # t=['to', 'be', 'or', 'not', 'to', 'be'].count('to')
-# # print(t)
-#
-# #extend
-# # a = [1, 2, 3]
-# # b = [4, 5, 6]
-# # a.extend(b)
-# # print(a)
-# # print(b)
-#
-# # index #  根据内容找位置
-#
-# # a = ['wuchao', 'jinxin', 'xiaohu','ligang',
-#  'sanpang', 'ligang', ['wuchao', 'jinxin']]
-# #
-# #
-# # first_lg_index = a.index("ligang")
-# # print("first_lg_index",first_lg_index)
-# # little_list = a[first_lg_index+1:]
-# #
-# # second_lg_index = little_list.index("ligang")
-# # print("second_lg_index",second_lg_index)
-# #
-# # second_lg_index_in_big_list = first_lg_index + second_lg_index +1
-# #
-# # print("second_lg_index_in_big_list",second_lg_index_in_big_list)
-# # print("second lg:",a[second_lg_index_in_big_list])
-#
-# # reverse   该方法没有返回值  倒序排序
-#
-# # a = ['wuchao', 'jinxin', 'xiaohu','ligang', 'sanpang', 'ligang']
-# # a.reverse()
-# # print(a)
-#
-# # x = [4, 6, 2, 1, 7, 9]
-# # x.sort(reverse=True)  该方法没有返回值
-# # print(x)#[1, 2, 4, 6, 7, 9]
-#
-# #
-# a = ['wuchao', 'jinxin', 'Xiaohu','Ligang', 'sanpang', 'ligang']
-# print(a.count("wuchao"))
-# # # a.sort()
-# # # print(a)
-# #
-# # print(a.count("haidilao ge"))
-# # print(a.pop())
-# # print(a)
+#print(type(a[0:]))#取到最后
+#print(a[1:-1])#取到倒数第二值
+#print(a[1:-1:1])#传递给后面从一开始
+#print(a[1::2])#从左到右隔一个去取 另立门户
+#print(a[3::-1])#翻转过来取值
+#print(a[-2::-1])#-1是倒过来取值
+#添加append insert
+#a.append('xuepeng')#默认插到最后一个位置
+#a.insert(1,'xuepeng') #将数据插入到任意一个位置
+#修改
+#a[1]='haidilao'
+#a[1:3]=['a','b']
+#print(a)
+#删除 remove pop del
+#c=["a","a","a","a"]
+#c.remove("a") #只删除一个
+#a.remove(a[0])
+#a.pop(1)#会返回删除的值
+#del a[0]
+#del a
+#count:计算某元素出现次数
+#t=['to', 'be', 'or', 'not', 'to', 'be'].count('to')
+#extend
+#a = [1, 2, 3]
+#b = [4, 5, 6]
+#a.extend(b)
+#index根据内容找位置
+#a = ['wuchao', 'jinxin', 'xiaohu','ligang',['wuchao', 'jinxin']]
+#first_lg_index = a.index("ligang")
+#print("first_lg_index",first_lg_index)
+#little_list = a[first_lg_index+1:]
+#second_lg_index = little_list.index("ligang")
+#print("second_lg_index",second_lg_index)
+#second_lg_index_in_big_list = first_lg_index + second_lg_index +1
+#print("second_lg_index_in_big_list",second_lg_index_in_big_list)
+#print("second lg:",a[second_lg_index_in_big_list])
+#reverse该方法没有返回值  倒序排序
+#a = ['wuchao', 'jinxin', 'xiaohu','ligang', 'sanpang', 'ligang']
+#a.reverse()
+#x = [4, 6, 2, 1, 7, 9]
+#x.sort(reverse=True)  该方法没有返回值
