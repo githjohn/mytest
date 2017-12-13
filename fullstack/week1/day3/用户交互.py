2f88358b043b987e322fd487e0d02747e5767037
githjohn <1693886289@qq.com>
2017-12-13 20:18:12 +0800

    交互

--- fullstack/week1/day3/用户交互.py
+++ fullstack/week1/day3/用户交互.py
@@ -1,19 +1,16 @@
-# #coding:utf-8 python3默认是utf-8  默认是3个字节
-# #要是python2  就要設置 #! -*- coding:gbk -*-
-# age_of_princal = 56
-# guess_age = int(   input(">>:") )
+#coding:utf-8 #python3默认是utf-8  默认是3个字节
+##要是python2  就要設置 #! -*- coding:gbk -*-
+#age_of_princal = 56
+# guess_age = int(   input(">>:") )#input 接受的所有数据都是字符串，即便你输入的是数字，但依然会被当成字符串来处理
 # if guess_age == age_of_princal:
 #     print("Yes,you got it..")
 # elif guess_age > age_of_princal:
 #     print("shoud try samller..")
 # else:
 #     print("try bigger ...")
-# age = input("your age:")  #input 接受的所有数据都是字符串，即便你输入的是数字，但依然会被当成字符串来处理
 # print( type(age) )  #查看类型
 # #int integer =整数  把字符串转成int,用int(被转的数据)
 # #str string =字符串 把数据转成字h符串用str(被转的数据)
 # print("Your name:",name)
-# #拼接用+必須是字符串才可以.
-
-
+# #拼接用+必須是字符串才可以
 
