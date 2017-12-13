8c7a80b7a6b6d67e343abdb83cf3dea4e64363bd
githjohn <1693886289@qq.com>
2017-12-13 20:21:42 +0800

    Update 格式化输出.py

--- fullstack/week2/day1/格式化输出.py
+++ fullstack/week2/day1/格式化输出.py
@@ -1,17 +1,9 @@
-#__author:  Administrator
-#date:  2016/8/22
-
 name = input("Name:")
 age = int(input("Age:"))
 job = input("Job:")
 salary = input("Salary:")
-
 if salary.isdigit(): #长的像不像数字,比如200d , '200'
     salary = int(salary)
-# else:
-#     #print()
-#     exit("must input digit") #退出程序
-
 msg = '''
 --------- info of %s --------
 Name: %s
@@ -21,5 +13,4 @@ Salary: %f
 You will be retired in %s years
 -------- end ----------
 ''' % (name,name ,age ,job ,salary, 65-age )
-
 print(msg)
