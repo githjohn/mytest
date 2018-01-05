6f3f164ff45330ba7a8b6bf52d9f7f731782c425
githjohn <1693886289@qq.com>
2018-01-05 21:50:44 +0800

    Update list_lesson.py

--- fullstack/week2/day1/list_lesson.py
+++ fullstack/week2/day1/list_lesson.py
@@ -1,19 +1,20 @@
-#a=['wuchao','jinxin','xiaohu','sanpang','ligang']
-# #-1是一直取到低
-# #查  切片 []
-#print(type(a[0:]))#取到最后
-#print(a[1:-1])#取到倒数第二值
-#print(a[1:-1:1])#传递给后面从一开始
-#print(a[1::2])#从左到右隔一个去取 另立门户
-#print(a[3::-1])#翻转过来取值
-#print(a[-2::-1])#-1是倒过来取值
+
+#---列表
+#a=[1,2,3,4]
+#print(a[-1:])#最后一个数字[4]
+#print(a[0:])#从0位置开始取到最后[1, 2, 3, 4]
+#print(a[1:-1])#位置1开始取 -1最后一个位置不取[2, 3]
+#print(a[1:-1:1])#传递给后面从一开始[2, 3]
+#print(a[1::2])#从左到右隔一个去取 另立门户 [2, 4]
+#print(a[3::-1])#翻转过来取值[4, 3, 2, 1]
+#print(a[-2::-1])#-1是倒过来取值 [3, 2, 1]
 #添加append insert
 #a.append('xuepeng')#默认插到最后一个位置
 #a.insert(1,'xuepeng') #将数据插入到任意一个位置
 #修改
 #a[1]='haidilao'
-#a[1:3]=['a','b']
-#print(a)
+#a[1:3]=['a','b'] #从第一个位置开始到第二个位置修改 最后一个不算
+#print(a)#[1, 'a', 'b', 4]
 #删除 remove pop del
 #c=["a","a","a","a"]
 #c.remove("a") #只删除一个
@@ -31,14 +32,28 @@
 #a = ['wuchao', 'jinxin', 'xiaohu','ligang',['wuchao', 'jinxin']]
 #first_lg_index = a.index("ligang")
 #print("first_lg_index",first_lg_index)
-#little_list = a[first_lg_index+1:]
-#second_lg_index = little_list.index("ligang")
-#print("second_lg_index",second_lg_index)
-#second_lg_index_in_big_list = first_lg_index + second_lg_index +1
-#print("second_lg_index_in_big_list",second_lg_index_in_big_list)
-#print("second lg:",a[second_lg_index_in_big_list])
 #reverse该方法没有返回值  倒序排序
 #a = ['wuchao', 'jinxin', 'xiaohu','ligang', 'sanpang', 'ligang']
 #a.reverse()
-#x = [4, 6, 2, 1, 7, 9]
-#x.sort(reverse=True)  该方法没有返回值
+# x = [4, 6, 2, 1, 7, 9]
+# x.sort(reverse=True)#  该方法没有返回值
+# print(x)
+# for i in range(1,101,2): #2 步长
+#     print("loop:",i)
+#-------格式化输出
+# name = input("Name:")
+# age = int(input("Age:"))
+# job = input("Job:")
+# salary = input("Salary:")
+# if salary.isdigit(): #长的像不像数字,比如200d , '200'
+#     salary = int(salary)
+# msg = '''
+# --------- info of %s --------
+# Name: %s
+# Age : %d
+# Job : %s
+# Salary: %f
+# You will be retired in %s years
+# -------- end ----------
+# ''' % (name,name ,age ,job ,salary, 65-age )
+# print(msg)
